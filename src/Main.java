3a48088864bf7bd97744148bff5b67f16029dfcb
Cristian Mercado <crismer1988@gmail.com>
2023-01-27 21:35:51 -0500

    Ejercicios_Tema_8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona = new Persona();
+
+        persona.setEdad(34);
+        persona.setName("Cristian");
+        persona.setTelefono("305 7660000");
+
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getTelefono());
+    }
+}
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad(int valor) {
+        this.edad = valor;
+    }
+
+    public void setName(String valor) {
+        this.nombre = valor;
+    }
+
+    public void setTelefono(String valor) {
+        this.telefono = valor;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
     }
 }
